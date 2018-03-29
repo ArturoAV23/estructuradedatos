d320621544fb837d29bed1f914c5e0f7f75de028
ArturoAV23 <36938026+ArturoAV23@users.noreply.github.com>
2018-03-28 20:14:00 -0400

    Create cargar.h

---
+++ cargar.h
@@ -0,0 +1,14 @@
+#pragma once
+class cargar
+{
+private :
+	int v[100],n;
+public:
+	cargar(void);
+	~cargar(void);
+	void cargarvec(int v[],int n);
+	void mostrarvec(int v[],int n);
+	void ordenarvec(int v[],int n);
+
+};
+
